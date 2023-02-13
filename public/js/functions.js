2072e29e022b33a86a934a272b18034d970136b8
Leonard Menzel <leonderdunna@gmail.com>
2023-02-13 23:40:44 +0100

    Video und Fragen innerhalb der Hauptseite

---
+++ public/js/functions.js
@@ -0,0 +1,339 @@
+const model_uuid = {
+    cloze: "05995762-aaef-11ed-9c2a-237115d9c292",
+    basic: "58b7eb48-a586-11ed-a34c-9bbd37c88a3c"
+}
+const note_models = [
+    {
+        "__type__": "NoteModel",
+        "crowdanki_uuid": "58b7eb48-a586-11ed-a34c-9bbd37c88a3c",
+        "css": ".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n",
+        "flds": [
+            {
+                "collapsed": false,
+                "description": "",
+                "font": "Arimo",
+                "media": [],
+                "name": "Front",
+                "ord": 0,
+                "plainText": false,
+                "rtl": false,
+                "size": 20,
+                "sticky": false
+            },
+            {
+                "collapsed": false,
+                "description": "",
+                "font": "Arial",
+                "media": [],
+                "name": "Back",
+                "ord": 1,
+                "plainText": false,
+                "rtl": false,
+                "size": 20,
+                "sticky": false
+            }
+        ],
+        "latexPost": "\\end{document}",
+        "latexPre": "\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n",
+        "latexsvg": false,
+        "name": "Basic",
+        "req": [
+            [
+                0,
+                "any",
+                [
+                    0
+                ]
+            ]
+        ],
+        "sortf": 0,
+        "tags": [],
+        "tmpls": [
+            {
+                "afmt": "{{FrontSide}}\n\n<hr id=answer>\n\n{{Back}}",
+                "bafmt": "",
+                "bfont": "",
+                "bqfmt": "",
+                "bsize": 0,
+                "did": null,
+                "name": "Card 1",
+                "ord": 0,
+                "qfmt": "{{Front}}"
+            }
+        ],
+        "type": 0,
+        "vers": []
+    },
+    {
+        "__type__": "NoteModel",
+        "crowdanki_uuid": "05995762-aaef-11ed-9c2a-237115d9c292",
+        "css": ".card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n\n.cloze {\n font-weight: bold;\n color: blue;\n}\n.nightMode .cloze {\n color: lightblue;\n}",
+        "flds": [
+            {
+                "collapsed": false,
+                "description": "",
+                "font": "Arial",
+                "media": [],
+                "name": "Text",
+                "ord": 0,
+                "plainText": false,
+                "rtl": false,
+                "size": 20,
+                "sticky": false
+            }
+        ],
+        "latexPost": "\\end{document}",
+        "latexPre": "\\documentclass[12pt]{article}\n\\special{papersize=3in,5in}\n\\usepackage[utf8]{inputenc}\n\\usepackage{amssymb,amsmath}\n\\pagestyle{empty}\n\\setlength{\\parindent}{0in}\n\\begin{document}\n",
+        "latexsvg": false,
+        "name": "Cloze",
+        "req": [
+            [
+                0,
+                "any",
+                [
+                    0
+                ]
+            ]
+        ],
+        "sortf": 0,
+        "tags": [],
+        "tmpls": [
+            {
+                "afmt": "{{cloze:Text}}",
+                "bafmt": "",
+                "bfont": "",
+                "bqfmt": "",
+                "bsize": 0,
+                "did": null,
+                "name": "Cloze",
+                "ord": 0,
+                "qfmt": "{{cloze:Text}}"
+            }
+        ],
+        "type": 1,
+        "vers": []
+    }
+]
+let main_textFile = null
+const deck_configurations = [
+    {
+        "__type__": "DeckConfig",
+        "autoplay": true,
+        "buryInterdayLearning": true,
+        "crowdanki_uuid": "58b6fa76-a586-11ed-a34c-9bbd37c88a3c",
+        "dyn": false,
+        "interdayLearningMix": 0,
+        "lapse": {
+            "delays": [
+                10.0
+            ],
+            "leechAction": 1,
+            "leechFails": 8,
+            "minInt": 1,
+            "mult": 0.0
+        },
+        "maxTaken": 60,
+        "name": "Default",
+        "new": {
+            "bury": true,
+            "delays": [
+                1.0,
+                10.0
+            ],
+            "initialFactor": 2500,
+            "ints": [
+                1,
+                4,
+                0
+            ],
+            "order": 1,
+            "perDay": 10,
+            "separate": true
+        },
+        "newGatherPriority": 1,
+        "newMix": 1,
+        "newPerDayMinimum": 0,
+        "newSortOrder": 0,
+        "replayq": true,
+        "rev": {
+            "bury": true,
+            "ease4": 1.3,
+            "fuzz": 0.05,
+            "hardFactor": 1.2,
+            "ivlFct": 1.0,
+            "maxIvl": 36500,
+            "minSpace": 1,
+            "perDay": 9999
+        },
+        "reviewOrder": 0,
+        "timer": 0
+    }
+]
+const deck_config_uuid = "58b6fa76-a586-11ed-a34c-9bbd37c88a3c"
+
+const example_cards = [
+    {
+        "_id": "63e80265ab7b5ef8326690cf",
+        "id": "anki-addon-5889320815654693",
+        "deck_id": "2ITUIJhiRMQ",
+        "type": "Basic",
+        "question": "Was macht Substitution in der Prädikatenlogik?",
+        "answer": "\\( S = \\{ x \\Rightarrow s,y \\Rightarrow t \\} \\) <br /> diese Substitution \\( S \\) ersetzt in einer Formel, alle freien \\( x \\) und \\(y\\) durch \\(s\\) und \\(t\\)",
+        "time": 0,
+        "__v": 0
+    }, {
+        "_id": "63e80510ab7b5ef8326690e7",
+        "id": "anki-addon-3647871503801243",
+        "deck_id": "2ITUIJhiRMQ",
+        "type": "Basic",
+        "question": "Was bedeutet \"Kollisionsfreiheit\" in Bezug auf Substitutionen in  der Prädikatenlogik?",
+        "answer": "Es existieren keine freien Variablen, auf die die Substitution abbildet.",
+        "time": 0,
+        "__v": 0
+    }]
+let cards = []
+let cards_ready = false
+let deck_struct = []
+const example_deck_structure = {
+    name: "Informatik",
+    uuid: 12345,
+    ids: [],
+    children: [{
+        name: "Aussagenlogik",
+        uuid: 34567,
+        ids: ["2ITUIJhiRMQ"],
+        children: [{
+            name: "alpharegeln",
+            uuid: 2345,
+            ids: [],
+            children: []
+        }]
+    }, {
+        name: "Prädikatenlogik",
+        uuid: "ne",
+        ids: [],
+        children: []
+    }]
+}
+const server = {
+    cards: "https://139-162-135-50.ip.linodeusercontent.com:80/card/",
+    decks: "https://139-162-135-50.ip.linodeusercontent.com:80/deck/"
+}
+
+function getEmptyCrowdAnkiDeck(name, uuid) {
+    return {
+        __type__: "Deck",
+        children: [],
+        crowdanki_uuid: uuid,
+        deck_config_uuid: deck_config_uuid,
+        media_files: [],
+        name: name,
+        notes: []
+    }
+}
+
+function makeDeckBaseDeck(deck) {
+    deck.deck_configurations = deck_configurations
+    deck.note_models = note_models
+    return deck
+}
+
+function cardToNote(card) {
+    let note = {
+        __type__: "Note",
+        guid: card.id,
+        tags: []
+    }
+    if (card.type === "Basic") {
+        note.note_model_uuid = model_uuid.basic
+        note.fields = [card.question, card.answer]
+    }
+    if (card.type === "Cloze") {
+        note.note_model_uuid = model_uuid.cloze
+        note.fields = [card.text]
+    }
+    return note
+}
+
+function deckStructureToCrowdAnki(deck) {
+    let ca = getEmptyCrowdAnkiDeck(deck.name, deck.uuid)
+    for (let id of deck.ids) {
+        let notes = cards.filter((e) => {
+            return e.deck_id === id;
+        }).map(e => cardToNote(e))
+        ca.notes.push(...notes)
+    }
+    for (let child of deck.children) {
+        ca.children.push(deckStructureToCrowdAnki(child))
+    }
+    return ca
+
+}
+
+function downloadFile(filePath) {
+    //Kopiert von:https://stackoverflow.com/questions/1066452/easiest-way-to-open-a-download-window-without-navigating-away-from-the-page
+    let link = document.createElement('a');
+    link.href = filePath;
+    link.download = "AnkiTube.json";
+    link.click();
+}
+
+function makeTextFile(text) {
+    let data = new Blob([text], {type: 'text/plain'});
+
+    // If we are replacing a previously generated file we need to
+    // manually revoke the object URL to avoid memory leaks.
+    if (main_textFile !== null) {
+        window.URL.revokeObjectURL(main_textFile);
+    }
+
+    main_textFile = window.URL.createObjectURL(data);
+
+    return main_textFile;
+}
+
+function removeDeck(deck) {
+    for (let d of deck_struct) {
+        if (d.uuid === deck.uuid) {
+            deck_struct.splice(deck_struct.indexOf(deck), 1)
+            fetch(server.decks + d.uuid, {
+                method: "DELETE"
+            }).then(r => r.json()).then((res)=>{
+                console.log(res)
+            })
+        }
+        removeDeckRecursive(d, deck)
+    }
+}
+
+function removeDeckRecursive(parent, toDelete) {
+    for (let d of parent.children) {
+        if (d.uuid === toDelete.uuid) {
+            parent.children.splice(parent.children.indexOf(toDelete), 1)
+            fetch(server.decks, {
+                method: "POST", headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(getBaseDeck(parent.uuid))
+            }).then(r => r.json()).then((res) => {
+                console.log(res)
+            })
+        }
+        removeDeckRecursive(d, toDelete)
+    }
+}
+
+function getBaseDeck(uuid) {
+    for (let deck of deck_struct) {
+        if (deckContains(uuid, deck))
+            return deck
+    }
+    console.error("Deck konnte nicht gefunden werden")
+    return false
+}
+
+function deckContains(uuid, deck) {
+    if (deck.uuid === uuid) return true
+    for (let subdeck of deck.children) {
+        if (deckContains(uuid, subdeck)) return true
+    }
+    return false
+}
---
+++ public/js/gui.js
@@ -0,0 +1,194 @@
+function main() {
+    let addDiv = document.createElement("div")
+    let addLabel = document.createElement("button")
+    addLabel.classList.add("add")
+    addDiv.classList.add("deck")
+    addLabel.textContent = "+"
+    addLabel.addEventListener("click", () => {
+        let name = prompt("Deckname")
+        if (name != null && name !== "") {
+            let deck = {
+                name: name,
+                uuid: Math.random(),
+                children: [],
+                ids: []
+            }
+            fetch(server.decks, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(deck)
+            }).then(r => r.json()).then((res) => {
+                console.log(res)
+                deck_struct.push(deck)
+                document.body.append(getDeckDiv(deck))
+            })
+
+
+        }
+    })
+    addDiv.append(addLabel)
+
+    document.body.append(addDiv)
+    document.getElementById("download-extension-link").addEventListener("click",()=>{
+        window.open("./AnkiTube.zip")
+    })
+    document.getElementById("reload-link").addEventListener("click",()=>{
+        location.reload()
+    })
+    document.getElementById("howto-link").addEventListener("click",()=>{
+        window.open("./howto.html")
+    })
+    document.getElementById("report-link").addEventListener("click",()=>{
+        window.open("https://github.com/leonderdunna/AnkiTube/issues")
+    })
+
+    fetch(server.cards).then(r => r.json())
+        .then((c) => {
+            cards = c
+            cards_ready = true
+
+        })
+    fetch(server.decks).then(r => r.json())
+        .then((decks) => {
+            deck_struct = decks
+            for (let deck of deck_struct) {
+                document.body.append(getDeckDiv(deck))
+            }
+        })
+
+}
+
+function getDeckDiv(deck) {
+    let deckDiv = document.createElement("div")
+    deckDiv.classList.add("deck", deck.uuid)
+    let name = document.createElement("span")
+    name.textContent = deck.name
+    deckDiv.append(name)
+
+    let addDeckButton = document.createElement("button")
+    addDeckButton.textContent = "+ Deck"
+    addDeckButton.addEventListener("click", () => {
+        let name = prompt("Deckname")
+        if (name != null && name !== "") {
+            deck.children.push({
+                name: name,
+                uuid: Math.random(),
+                ids: [],
+                children: []
+            })
+            console.log(deck)
+            fetch(server.decks, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(getBaseDeck(deck.uuid))
+            }).then(r => r.json()).then((res) => {
+                console.log(res)
+                let p = document.getElementsByClassName(deck.uuid)[0].parentElement
+                document.getElementsByClassName(deck.uuid)[0].remove()
+                p.append(getDeckDiv(deck))
+            })
+
+
+        }
+    })
+    deckDiv.append(addDeckButton)
+
+    let addVideoButton = document.createElement("button")
+    addVideoButton.textContent = "+ Video"
+    addVideoButton.addEventListener("click", () => {
+        let id = prompt("Video ID")
+        if (id != null && id !== "") {
+            deck.ids.push(id)
+            console.log(deck)
+            fetch(server.decks, {
+                method: "POST",
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(getBaseDeck(deck.uuid))
+            }).then(r => r.json()).then((res) => {
+                console.log(res)
+                let p = document.getElementsByClassName(deck.uuid)[0].parentElement
+                document.getElementsByClassName(deck.uuid)[0].remove()
+                p.append(getDeckDiv(deck))
+            })
+        }
+    })
+    deckDiv.append(addVideoButton)
+
+    let deletebutton = document.createElement("button")
+    deletebutton.textContent = "X"
+    deletebutton.addEventListener("click", () => {
+        deckDiv.remove()
+        removeDeck(deck)
+    })
+    deckDiv.append(deletebutton)
+
+    let downloadbutton = document.createElement("button")
+    downloadbutton.textContent = "↓"
+    downloadbutton.addEventListener("click", () => {
+        downloadFile(makeTextFile(JSON.stringify(makeDeckBaseDeck(deckStructureToCrowdAnki(deck)))))
+    })
+    deckDiv.append(downloadbutton)
+
+
+    if (deck.ids.length > 0) {
+
+        let ids = document.createElement("details")
+        let ids_name = document.createElement("summary")
+        ids_name.textContent = "Videos: " + deck.ids.length
+        ids.append(ids_name)
+        let id_ul = document.createElement("ul")
+        for (let id of deck.ids) {
+            let i = document.createElement("li")
+            i.textContent = id
+            let löschenbutton = document.createElement("button")
+            löschenbutton.textContent = "X"
+            löschenbutton.addEventListener("click", () => {
+                deck.ids.splice(deck.ids.indexOf(id), 1)
+                i.remove()
+
+                fetch(server.decks, {
+                    method: "POST",
+                    headers: {'Content-Type': 'application/json'},
+                    body: JSON.stringify(getBaseDeck(deck.uuid))
+                }).then(r => r.json()).then((res) => {
+
+                    ids_name.textContent = "Videos: " + deck.ids.length
+                    if (deck.ids.length === 0)
+                        ids.remove()
+                })
+            })
+            i.append(löschenbutton)
+
+
+            let editButton = document.createElement("button")
+            editButton.textContent = "Bearbeiten"
+            editButton.addEventListener("click",()=>{
+                document.getElementsByClassName("card-edit-main")[0]?.remove()
+                document.body.append(getMainPanel("AnkiTube",id))
+            })
+            i.append(editButton)
+
+            id_ul.append(i)
+        }
+        ids.append(id_ul)
+        deckDiv.append(ids)
+    }
+
+    if (deck.children.length > 0) {
+        let subDeckUl = document.createElement("ul")
+        for (let subdeck of deck.children) {
+            let subdeckli = document.createElement("li")
+            subdeckli.append(getDeckDiv(subdeck))
+            subDeckUl.append(subdeckli)
+        }
+        deckDiv.append(subDeckUl)
+    }
+
+
+    return deckDiv
+
+
+}
+
+
+main()
